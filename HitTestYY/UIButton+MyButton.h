833a0f6a824bb966477a6b109cff3fec5a9f4fc6
yang99 <996212017@qq.com>
2015-10-20 19:24:00 +0800

    first commit

---
+++ HitTestYY/UIButton+MyButton.h
@@ -0,0 +1,13 @@
+//
+//  UIButton+MyButton.h
+//  HitTestYY
+//
+//  Created by yangyao on 15/10/20.
+//  Copyright © 2015年 yangyao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIButton (MyButton)
+
+@end
---
+++ HitTestYY/UIButton+MyButton.m
@@ -0,0 +1,13 @@
+//
+//  UIButton+MyButton.m
+//  HitTestYY
+//
+//  Created by yangyao on 15/10/20.
+//  Copyright © 2015年 yangyao. All rights reserved.
+//
+
+#import "UIButton+MyButton.h"
+
+@implementation UIButton (MyButton)
+
+@end
